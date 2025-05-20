c5beea132555aa7da0054fa6885e93b9482740c8
Rafael Carvalho <az.carta@gmail.com>
2025-05-20 18:56:58 -0300

    chore: resolve TODO

--- lib/netflixir/videos/stream_packager.ex
+++ lib/netflixir/videos/stream_packager.ex
@@ -12,15 +12,11 @@ defmodule Netflixir.Videos.StreamPackager do
   alias Netflixir.Videos.Processors.ResolutionProcessor
   alias Netflixir.Videos.Processors.Transcoder
 
-  # TODO: Remove the example default value for raw_video once everything
-  # is working.
-  def process_video(raw_video \\ "priv/static/videos/raw/cat_rave.mp4") do
+  def process_video(raw_video) do
     with {:ok, transcoded_video} <- Transcoder.transcode_raw_video(raw_video),
          {:ok, resolutions_dir} <-
            ResolutionProcessor.create_video_resolutions(transcoded_video),
          {:ok, video_segments_dir} <- HlsProcessor.create_video_segments(resolutions_dir) do
-      cleanup_unused_files([transcoded_video])
-
       {:ok,
        %{
          video_segments_dir: video_segments_dir,
@@ -28,9 +24,5 @@ defmodule Netflixir.Videos.StreamPackager do
          resolutions_dir: resolutions_dir
        }}
     end
-  end
-
-  defp cleanup_unused_files(files) do
-    tap(files, &File.rm/1)
   end
 end
