cb237d186a774291e3c487871c8e93255030a9d9
Rafael Carvalho <az.carta@gmail.com>
2025-07-18 15:16:39 -0300

    fix: update database hostname to localhost in development and test configurations

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :netflixir, Netflixir.Repo,
   username: System.get_env("POSTGRES_USER", "postgres"),
   password: System.get_env("POSTGRES_PASSWORD", "postgres"),
-  hostname: System.get_env("POSTGRES_HOST", "postgres"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   database: System.get_env("POSTGRES_DB", "netflixir_dev"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
